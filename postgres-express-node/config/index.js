a77c100b685aa37ba18ff74c9018873d342b220a
tamiib <tamibarisic28@gmail.com>
2020-11-11 13:57:50 +0100

    Refactor login to issue JWT tokens

--- postgres-express-node/config/index.js
+++ postgres-express-node/config/index.js
@@ -1,18 +1,15 @@
+//index.js u config
 const dotenv = require("dotenv");
-
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("Couldn't find .env file");
 }
-
 /**
  * Express app config
  */
 module.exports = {
   port: process.env.PORT,
-
   // For winston and morgan loggers
   logs: {
     winston: {
@@ -22,8 +19,11 @@ module.exports = {
       format: process.env.MORGAN_FORMAT || "combined",
     },
   },
-
   api: {
     prefix: "/api",
   },
+  jwt:{
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_DURATION || "1h",
+  }
 };
--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,15 +1,49 @@
+//login.service.js
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
     this.logger = logger;
   }
-
   async getUser(userDTO) {
     const user = await this.userModel.findOne({
       where: userDTO,
     });
     return user;
   }
+  async login({username, password}) {
+    console.log(username);
+    const userRecord = await this.userModel.findOne({
+      where: {username},
+    })
+    console.log(userRecord);
+    if(!userRecord){
+      this.logger.error("User not registered");
+      throw new error("Authentication failed");
+    }
+    
+    this.logger.info("Checking password");
+    if(userRecord.password === password){
+      this.logger.info("Password correct");
+      const user = {
+        username: userRecord.username,
+        role: userRecord.role || "guest",
+      };
+      const payload = {
+        ...user,
+        aud: config.jwt.audience || "localhost/api",
+        iss: config.jwt.issuer || "localhost@fesb",
+      };
+      
+      const token = this.generateToken(payload);
+      return {user, token};
+    }
+  }
+  generateToken(payload){
+    return jwt.sign(payload, config.jwt.secret, {
+      expiresIn: config.jwt.expiresIn,
+    });
+  }
 }
-
 module.exports = LoginService;
